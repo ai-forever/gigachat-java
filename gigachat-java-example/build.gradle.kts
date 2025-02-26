6b93dbe7ba8ae3f5d97d431bfd876ac053050e29
Sergey Safonov <ssafonov@sber.ru>
2025-02-26 11:26:28 +0300

    basic interface and auth call logic

--- gigachat-java-example/build.gradle.kts
+++ gigachat-java-example/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":gigachat-java"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- gigachat-java-example/src/main/java/chat/giga/Main.java
+++ gigachat-java-example/src/main/java/chat/giga/Main.java
@@ -1,7 +1,18 @@
 package chat.giga;
 
+import chat.giga.client.GigaChatAuthClient;
+import chat.giga.client.GigaChatAuthClientImpl;
+import chat.giga.model.Scope;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        GigaChatClient client = ClientBuilder.token().clientId().secret().scope().build();
+//        client.embeddings()
+
+        GigaChatAuthClient client = new GigaChatAuthClientImpl();
+        client.oauth("680f53b4-936b-4e10-ac12-427354dbd90c", "6ccd7d51-39ee-443f-8412-b8438114583c", Scope.GIGACHAT_API_PERS);
+
+
     }
 }
