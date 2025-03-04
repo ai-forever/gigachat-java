cf589305ab3e771b2e4ed750d4e5cfa66abdf933
Sergey Safonov <ssafonov@sber.ru>
2025-03-04 12:33:46 +0300

    add examples

---
+++ gigachat-java-example/src/main/java/chat/giga/ModelListExample.java
@@ -0,0 +1,22 @@
+package chat.giga;
+
+import chat.giga.client.GigaChatClient;
+import chat.giga.http.client.HttpClientException;
+import chat.giga.model.Scope;
+
+public class ModelListExample {
+
+    public static void main(String[] args) {
+
+        GigaChatClient client = GigaChatClient.builder()
+                .clientId("test-client-id")
+                .clientSecret("test-scope")
+                .scope(Scope.GIGACHAT_API_PERS)
+                .build();
+        try {
+            System.out.println(client.models());
+        } catch (HttpClientException ex) {
+            System.out.println(ex.statusCode() + " " + ex.bodyAsString());
+        }
+    }
+}
