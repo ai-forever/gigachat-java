be7c51600ed28e424ea87e1991e94c831a282b72
Бочарова Дарья Михайловна <DMBocharova@sberbank.ru>
2025-03-05 18:56:12 +0300

    ci, publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     java
+    `maven-publish`
+    signing
 }
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
+    withJavadocJar()
+    withSourcesJar()
 }
 
 allprojects {
@@ -17,3 +21,50 @@ allprojects {
     }
 }
 
+signing {
+    sign(publishing.publications)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "gigachat-sdk-java"
+            version = project.version.toString()
+
+            pom {
+                name.set("GigaChat SDK Java")
+                url.set("https://github.com/ai-forever/gigachat-java")
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("name")
+                        name.set("name")
+                        email.set("name")
+                    }
+                }
+                scm {
+                    connection.set("scm:https://github.com/ai-forever/gigachat-java.git")
+                    developerConnection.set("scm:git@github.com:ai-forever/gigachat-java.git")
+                    url.set("https://github.com/ai-forever/gigachat-java")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "MavenCentral"
+            url = uri("https://s01.oss.sonatype.org/repository/maven-releases/")
+            credentials {
+                username = project.findProperty("OSSRH_USERNAME") as String? ?: ""
+                password = project.findProperty("OSSRH_PASSWORD") as String? ?: ""
+            }
+        }
+    }
+}
