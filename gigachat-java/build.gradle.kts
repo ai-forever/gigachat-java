8ce0a995f75c4273149f1dfced4aef1aa7572278
Sergey Safonov <ssafonov@sber.ru>
2025-03-07 15:59:47 +0300

    remove org.openapi.generator

--- gigachat-java/build.gradle.kts
+++ gigachat-java/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `java-library`
     `maven-publish`
     id("io.freefair.lombok") version "8.12.2"
-    id("org.openapi.generator") version "7.11.0"
 }
 
 publishing {
@@ -31,31 +30,5 @@ tasks.test {
 
 tasks.compileJava {
     options.encoding = "UTF-8"
-
-    dependsOn(tasks.openApiGenerate)
 }
 
-tasks.openApiGenerate {
-    dependsOn(tasks.processResources)
-}
-
-openApiGenerate {
-    generatorName.set("java")
-    inputSpec.set("${sourceSets.main.get().output.resourcesDir}/spec/api.yml")
-    outputDir.set("${layout.buildDirectory.dir("generated").get()}")
-    modelPackage.set("chat.giga.model")
-    generateModelDocumentation.set(false)
-    generateModelTests.set(false)
-    configOptions.set(
-            mapOf(
-                    "library" to "microprofile",
-                    "serializationLibrary" to "jackson",
-                    "generateBuilders" to "true"
-            )
-    )
-    globalProperties.set(
-            mapOf(
-                    "models" to ""
-            )
-    )
-}
