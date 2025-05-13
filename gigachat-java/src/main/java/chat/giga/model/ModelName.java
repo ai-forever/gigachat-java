13831774f2891c409aded6d8762dc18494d0010e
Sergey Safonov <ssafonov@sber.ru>
2025-05-13 10:04:28 +0300

    add model EMBEDDINGS_GIGA_R const

--- gigachat-java/src/main/java/chat/giga/model/ModelName.java
+++ gigachat-java/src/main/java/chat/giga/model/ModelName.java
@@ -7,5 +7,12 @@ public class ModelName {
     public static final String GIGA_CHAT_PRO_2 = "GigaChat-2-Pro";
     public static final String GIGA_CHAT_MAX = "GigaChat-Max";
     public static final String GIGA_CHAT_MAX_2 = "GigaChat-2-Max";
+    /**
+     * Базовая модель, доступная по умолчанию для векторного представления текстов
+     */
     public static final String EMBEDDINGS = "Embeddings";
+    /**
+     * Продвинутая модель с большим размером контекста
+     */
+    public static final String EMBEDDINGS_GIGA_R = "EmbeddingsGigaR";
 }
